c38cdcfd32870b6d14ea8241b2c067696a221650
Marjolaine Nuijten <marjoonuijten@hotmail.nl>
2016-11-06 12:21:41 +0100

    Added some initial game logic

--- src/js/game.js
+++ src/js/game.js
@@ -1,11 +1,72 @@
+// Third party plugins
 var _ = require('./plugins/lodash'),
 	$ = require('./plugins/jquery'),
+	// Mechanics
+	events = require('./mechanics/events'),
+	// Objects
 	Cookie = require('./objects/cookie'),
 	data = require('./objects/data'),
 	Hero = require('./objects/hero'),
 	Item = require('./objects/item'),
 	Level = require('./objects/level');
 
-var currentStage = 1;
+var currentStage,
+	levelData,
+	selectors = {
+		level: '.js-level'
+	},
+	instances = {};
 
-alert('Welcome to level ' + currentStage);
+/**
+ * [startLevel description]
+ * @param  {[type]} stage [description]
+ * @return {[type]}       [description]
+ */
+function startLevel(stage) {
+	levelData = _.find(
+		data.levels,
+		{ stage: stage }
+	);
+
+	var hero = new Hero(
+			levelData.hero.image,
+			levelData.hero.message
+		),
+		level = new Level(
+			$(selectors.level),
+			levelData.level.image
+		);
+
+	$.extend(instances, {
+		hero: hero,
+		level: level
+	});
+
+	level.draw();
+}
+
+/**
+ * [bindEvents description]
+ * @return {[type]} [description]
+ */
+function bindEvents() {
+	document.onkeydown = _.partial(events.onKeyDown, instances);
+}
+
+/**
+ * [init description]
+ * @return {[type]} [description]
+ */
+function init() {
+	currentStage = 1;
+
+	startLevel(currentStage);
+	bindEvents();
+}
+
+init();
+
+
+
+
+
