5967e9e8a1409a972b359ad1f9784f0a35e22f1b
Marjolaine Nuijten <marjoonuijten@hotmail.nl>
2016-11-06 12:08:35 +0100

    Console log testing movement functions hero

--- src/js/objects/hero.js
+++ src/js/objects/hero.js
@@ -1,10 +1,21 @@
-function Hero() {}
+function Hero(image, message) {
+	this.image = image;
+	this.message = message;
+}
 
 Hero.prototype = {
-	jump: function jump() {},
-	moveLeft: function moveLeft() {},
-	moveRight: function moveRight() {},
-	speak: function speak() {}
+	jump: function jump() {
+		console.log('Currently: jump');
+	},
+	moveLeft: function moveLeft() {
+		console.log('Currently: moveLeft');
+	},
+	moveRight: function moveRight() {
+		console.log('Currently: moveRight');
+	},
+	speak: function speak() {
+		return this.message;
+	}
 };
 
 module.exports = Hero;
